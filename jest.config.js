78d9b272fbb715ea5c02bf5243f391996fbb7329
dillon ofili <dillonofili667@gmail.com>
2026-08-04 11:39:44 +0100

    feat(api): initialize project scaffolding and typescript configuration

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleNameMapper: {
+    '^(\\..*)\\.js$': '$1'
+  },
+  clearMocks: true
+};
